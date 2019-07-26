7b6e557a5db0daf98d4592ae8516e1399bde4f3f
Cameron Ames <cameron.ames@live.com>
2019-07-26 11:32:41 -0400

    update executables

--- heroku-ci/scripts/test-setup.sh
+++ heroku-ci/scripts/test-setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export PATH="$BUILD_DIR/vendor/sfdx/cli/bin:$PATH"
+export PATH="$BUILD_DIR/vendor/sfdx/jq:$PATH"
 
 echo "------> Setting up test..."
 bash ./$CI_ASSET_DIR/scripts/create-scratch-org.sh || return 1
