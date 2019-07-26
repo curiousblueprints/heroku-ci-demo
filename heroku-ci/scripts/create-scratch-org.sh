d7057ce34a073a5a9973f2b915c3e070528402ef
Cameron Ames <cameron.ames@live.com>
2019-07-26 12:21:30 -0400

    update executables

--- heroku-ci/scripts/create-scratch-org.sh
+++ heroku-ci/scripts/create-scratch-org.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #create-scratch-org.sh
-export PATH="$BUILD_DIR/vendor/sfdx/cli/bin:$PATH"
-export PATH="$BUILD_DIR/vendor/sfdx/jq:$PATH"
-
-ls vendor/sfdx/jq
-ls vendor/sfdx
+export PATH="vendor/sfdx/cli/bin:$PATH"
+export PATH="vendor/sfdx/jq:$PATH"
 
 # Decrypt server.key. Used for JWT authorization.
 openssl aes-256-cbc -k $ENC_KEY -in $CI_ASSET_DIR/server.key.enc -out $CI_ASSET_DIR/server.key -d
