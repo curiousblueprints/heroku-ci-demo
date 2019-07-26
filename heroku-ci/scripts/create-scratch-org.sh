f092ca200eba9b087eeb6fbd7adc114db2fb44f1
Cameron Ames <cameron.ames@live.com>
2019-07-26 12:09:43 -0400

    update executables

--- heroku-ci/scripts/create-scratch-org.sh
+++ heroku-ci/scripts/create-scratch-org.sh
@@ -11,16 +11,10 @@ sfdx force:auth:jwt:grant --clientid $SF_CLIENT_ID --jwtkeyfile $CI_ASSET_DIR/se
 
 
 echo "Creating scratch org..."
-sfdx force:org:create --definitionfile $CI_ASSET_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json
-
-# Configure environment variables used in testing.
-ORG_JSON=$(sfdx force:org:display --json -u $SF_USERNAME)
-export SALESFORCE_ACCESS_TOKEN=$(echo $ORG_JSON | jq -r '.result.accessToken')
-export SALESFORCE_INSTANCE_URL=$(echo $ORG_JSON | jq -r '.result.instanceUrl')
-export SALESFORCE_ORG_ID=$(echo $ORG_JSON | jq -r '.result.id')
-export SALESFORCE_USERNAME=$(echo $ORG_JSON | jq -r '.result.username')
+export SALESFORCE_USERNAME=$(sfdx force:org:create --definitionfile $CI_ASSET_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username')
 
 # Delete the unencrypted key.
 rm $CI_ASSET_DIR/server.key
+
 # Optional: deploy metadata required by tests
 sfdx force:mdapi:deploy -d test_org/unpackaged --wait 1 -u $SALESFORCE_USERNAME
