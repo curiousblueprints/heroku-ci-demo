ae08713ca08ae041a0a882906d08bfbc8b3ee130
Cameron Ames <cameron.ames@live.com>
2019-07-27 11:43:07 -0400

    change parent child shell to source

--- heroku-ci/scripts/test-setup.sh
+++ heroku-ci/scripts/test-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "-----> Setting up test..."
-bash ./$CI_ASSET_DIR/scripts/export-paths.sh
-bash ./$CI_ASSET_DIR/scripts/decrypt-key.sh
-bash ./$CI_ASSET_DIR/scripts/auth-jwt-grant.sh
-bash ./$CI_ASSET_DIR/scripts/create-scratch-org.sh
+source ./$CI_ASSET_DIR/scripts/export-paths.sh
+source ./$CI_ASSET_DIR/scripts/decrypt-key.sh
+source ./$CI_ASSET_DIR/scripts/auth-jwt-grant.sh
+source ./$CI_ASSET_DIR/scripts/create-scratch-org.sh
 echo "-----> Done setting up test"
