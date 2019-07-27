4a942ee26544bf10393cf3f89604ef2082abde19
Cameron Ames <cameron.ames@live.com>
2019-07-27 17:23:53 -0400

    update

--- heroku-ci/scripts/deploy-metadata.sh
+++ heroku-ci/scripts/deploy-metadata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "       deploying metadata..."
+echo "$SALESFORCE_USERNAME"
 
 sfdx force:mdapi:deploy -d force-app/main/default --wait 1 -u $SALESFORCE_USERNAME --testlevel RunLocalTests
 
