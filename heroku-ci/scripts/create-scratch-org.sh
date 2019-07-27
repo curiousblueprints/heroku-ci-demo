041a76b5fa759e3e6e40db7381546cf39896c721
Cameron Ames <cameron.ames@live.com>
2019-07-27 13:11:19 -0400

    updat edeploy command

--- heroku-ci/scripts/create-scratch-org.sh
+++ heroku-ci/scripts/create-scratch-org.sh
@@ -5,4 +5,4 @@ echo "Creating scratch org..."
 export SALESFORCE_USERNAME=$(sfdx force:org:create --definitionfile $CI_ASSET_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username')
 
 # Optional: deploy metadata required by tests
-sfdx force:mdapi:deploy -d force-app/main/default --wait 1 -u $SALESFORCE_USERNAME --testLevel RunLocalTests
+sfdx force:mdapi:deploy -d force-app/main/default --wait 1 -u $SALESFORCE_USERNAME --testlevel RunLocalTests
