98ce53d9c96a2793129d3e2437c10bf5b82a0342
Cameron Ames <cameron.ames@live.com>
2019-07-27 13:27:12 -0400

    update dirs

--- heroku-ci/scripts/create-scratch-org.sh
+++ heroku-ci/scripts/create-scratch-org.sh
@@ -2,7 +2,7 @@
 
 
 echo "Creating scratch org..."
-export SALESFORCE_USERNAME=$(sfdx force:org:create --definitionfile $CI_ASSET_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username')
+export SALESFORCE_USERNAME=$(sfdx force:org:create --definitionfile $CONFIG_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username')
 
 # Optional: deploy metadata required by tests
 sfdx force:mdapi:deploy -d force-app/main/default --wait 1 -u $SALESFORCE_USERNAME --testlevel RunLocalTests
