0a6d779fa30b3a5777c70135d0916d90406547c0
Cameron Ames <cameron.ames@live.com>
2019-07-28 09:19:51 -0400

    add ignore warnings flag

--- heroku-ci/scripts/deploy-metadata.sh
+++ heroku-ci/scripts/deploy-metadata.sh
@@ -2,6 +2,6 @@
 
 echo "       deploying metadata..."
 
-sfdx force:mdapi:deploy -d force-app/main/default --wait 1 -u $SCRATCH_USERNAME --testlevel RunLocalTests
+sfdx force:mdapi:deploy -d force-app/main/default --wait 1 -u $SCRATCH_USERNAME --testlevel RunLocalTests --ignorewarnings
 
 echo "       done deploying metadata"
