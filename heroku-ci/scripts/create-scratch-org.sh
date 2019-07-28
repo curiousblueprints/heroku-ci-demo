1c0af02b84b5fc11597e8c644c508cf46584d8e8
Cameron Ames <cameron.ames@live.com>
2019-07-27 21:10:06 -0400

    test sfdx command err

--- heroku-ci/scripts/create-scratch-org.sh
+++ heroku-ci/scripts/create-scratch-org.sh
@@ -2,6 +2,6 @@
 
 echo "       creating scratch org..."
 
-export SCRATCH_USERNAME=$(sfdx force:org:create --definitionfile $CONFIG_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username')
-return -1
+export SCRATCH_USERNAME=$(sfdx force:org:create) #--definitionfile $CONFIG_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username')
+
 echo "       done creating scratch org"
