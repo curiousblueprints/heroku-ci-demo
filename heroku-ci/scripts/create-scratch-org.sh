9ac8c9304f69ab73aa242c35552773d5b8278118
Cameron Ames <cameron.ames@live.com>
2019-07-27 21:20:41 -0400

    command grouping

--- heroku-ci/scripts/create-scratch-org.sh
+++ heroku-ci/scripts/create-scratch-org.sh
@@ -2,6 +2,6 @@
 
 echo "       creating scratch org..."
 
-export SCRATCH_USERNAME=source sfdx force:org:create --definitionfile $CONFIG_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username'
+export SCRATCH_USERNAME= { source sfdx force:org:create --definitionfile $CONFIG_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username'; }
 
 echo "       done creating scratch org"
