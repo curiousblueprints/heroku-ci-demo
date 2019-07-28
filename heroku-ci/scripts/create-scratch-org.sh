cbedb87713b14b9c0091a32a928ac8bad42e7fdb
Cameron Ames <cameron.ames@live.com>
2019-07-27 21:29:27 -0400

    fix subshell

--- heroku-ci/scripts/create-scratch-org.sh
+++ heroku-ci/scripts/create-scratch-org.sh
@@ -2,6 +2,8 @@
 
 echo "       creating scratch org..."
 
-export SCRATCH_USERNAME= { source sfdx force:org:create --definitionfile $CONFIG_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username'; }
+{ source sfdx force:org:create --definitionfile $CONFIG_DIR/project-scratch-def.json --targetdevhubusername $SF_USERNAME --json | jq -r '.result.username'; } > scratch_username
+read -r scratch_username < scratch_username
+export SCRATCH_USERNAME="$scratch_username"
 
 echo "       done creating scratch org"
