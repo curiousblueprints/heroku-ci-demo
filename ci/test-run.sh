48ea1c1dc0743d800a43e22c422af304c909999f
Cameron Ames <cameron.ames@live.com>
2019-07-29 10:56:46 -0400

    fix spelling

--- ci/test-run.sh
+++ ci/test-run.sh
@@ -3,9 +3,9 @@ set -e
   
 echo "-----> running tests..."
 
-source ./$SCIRPT_DIR/export-buildpk-paths.sh
-source ./$SCIRPT_DIR/export-project-paths.sh
-source ./$SCIRPT_DIR/read-scratch-org-name.sh
-source ./$SCIRPT_DIR/deploy-metadata.sh
+source ./$SCRIPT_DIR/export-buildpk-paths.sh
+source ./$SCRIPT_DIR/export-project-paths.sh
+source ./$SCRIPT_DIR/read-scratch-org-name.sh
+source ./$SCRIPT_DIR/deploy-metadata.sh
 
 echo "-----> done running tests" 
--- ci/test-setup.sh
+++ ci/test-setup.sh
@@ -3,11 +3,11 @@ set -e
   
 echo "-----> setting up test..."
 
-source ./$SCIRPT_DIR/export-buildpk-paths.sh
-source ./$SCIRPT_DIR/decrypt-key.sh
-source ./$SCIRPT_DIR/auth-jwt-grant.sh
-source ./$SCIRPT_DIR/create-scratch-org.sh
-source ./$SCIRPT_DIR/remove-decrypted-key.sh
-source ./$SCIRPT_DIR/write-scratch-org-name.sh
+source ./$SCRIPT_DIR/export-buildpk-paths.sh
+source ./$SCRIPT_DIR/decrypt-key.sh
+source ./$SCRIPT_DIR/auth-jwt-grant.sh
+source ./$SCRIPT_DIR/create-scratch-org.sh
+source ./$SCRIPT_DIR/remove-decrypted-key.sh
+source ./$SCRIPT_DIR/write-scratch-org-name.sh
 
 echo "-----> done setting up test"
